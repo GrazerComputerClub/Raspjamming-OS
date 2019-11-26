c144db9fa2523991e4f7e71831f2671da6188c4f
mstroh <m.stroh@ymail.com>
2019-11-26 22:42:20 +0100

    Support for Ethernet connected Pis, Raspjamming 19.11

--- scripts/PiConnect.sh
+++ scripts/PiConnect.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "searching for raspberrypi.local... " 
 echo -n "Status: " 
 ping -c 1 raspberrypi.local > /dev/null 2>&1  
 if [ $? -eq 0 ]; then
@@ -14,10 +15,10 @@ echo Detecting connected network devices...
 for connection in $(nmcli d | egrep "  connected|  verbunden" | awk '{print $1;}') 
 do
   echo -n Probing connection ${connection}...
-  if [ $(ifconfig ${connection} | egrep "01:02:03:04:|enp0s20u" | wc -l) -gt 0 ]; then
+  if [ $(ifconfig ${connection} | egrep "01:02:03:04:|enp0s20u|192.168.138." | wc -l) -gt 0 ]; then
     RPiConnection=$connection
     LocalRPiIp=$(ifconfig $connection | grep "inet " | cut -d ':' -f 2 | cut -d ' ' -f 1)
-    echo "found Pi Zero with local IP: $LocalRPiIp"  
+    echo "found Raspberry Pi connected to local IP: $LocalRPiIp"  
   else
     # Hopefully there is just another connected device
     # Otherwise we asume that all connected devices except
@@ -28,7 +29,7 @@ do
 done
 
 if [ -z $RPiConnection ] ; then
-  echo Raspberry Pi Zero network device not found
+  echo Raspberry Pi network device not found
   exit
 fi
 
